abdf50a3575c6f4cfd0a55191e9d5a6df6ec503b
Luis Polina <luis.282001@gmail.com>
2022-11-14 13:55:11 -0600

    add tasks to the form

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,28 @@
+import TaskForm from "./TaskForm";
 import TaskList from "./TaskList";
+import { tasks as data } from "./tasks";
+import { useState, useEffect } from "react";
 
 function App() {
+  const [tasks, setTasks] = useState([]);
+
+  useEffect(() => {
+    setTasks(data);
+  }, []);
+
+function createTask(taskTitle) {
+  setTasks([...tasks, {
+    title: taskTitle,
+    id: tasks.length,
+    description: 'algo nuevo'
+  }])
+}
+
   return (
-    <div>
-      <TaskList />
-    </div>
+    <>
+      <TaskForm createTask={createTask}/>
+      <TaskList tasks={tasks} />
+    </>
   );
 }
 
--- src/TaskList.jsx
+++ src/TaskList.jsx
@@ -1,22 +1,13 @@
-import { tasks as data } from "./tasks";
-import { useState, useEffect } from "react";
-
-function TaskList() {
-  const [tasks, setTasks] = useState([]);
-
-  useEffect(() => {
-    setTasks(data)
-  }, [])
-  
-  if(tasks.length===0) {
-    return <h1>No hay tareas aún</h1>
+function TaskList({ tasks }) {
+  if (tasks.length === 0) {
+    return <h1>No hay tareas aún</h1>;
   }
   return (
     <div>
       {tasks.map((task) => (
         <div key={task.id}>
-            <h1>{task.title}</h1>
-            <p>{task.description}</p>
+          <h1>{task.title}</h1>
+          <p>{task.description}</p>
         </div>
       ))}
     </div>
